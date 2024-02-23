621c2a123507290846aac8157a105fd36b973536
jeevadev90 <152574226+jeevadev90@users.noreply.github.com>
2024-02-23 10:02:40 +0530

    Update CommentsController.java

--- src/main/java/com/backend/velanmai/Controller/CommentsController.java
+++ src/main/java/com/backend/velanmai/Controller/CommentsController.java
@@ -8,7 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@CrossOrigin(origins = "https://farming-0kkf.onrender.com")
+@CrossOrigin(origins = "https://letsfarming.netlify.app")
 @RequestMapping("/comments")
 public class CommentsController {
 
